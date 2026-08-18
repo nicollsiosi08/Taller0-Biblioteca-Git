dbca2285932afbf83f1e30f2c87c53aadfec2ece
njohanagomez <njohanagomez@unicesar.edu.co>
2026-08-18 08:59:06 -0500

    feat: implementar READ de Libro (buscar)

--- src/main/java/com/mycompany/taller0/biblioteca/git/Main.java
+++ src/main/java/com/mycompany/taller0/biblioteca/git/Main.java
@@ -131,4 +131,15 @@ public class Main {
             System.out.println(libro);
         }
     }
+    public static Libro buscarLibro(String codigo) {
+
+        for (Libro libro : libros) {
+
+            if (libro.getCodigo().equalsIgnoreCase(codigo)) {
+                return libro;
+            }
+        }
+
+        return null;
+    }
 }
